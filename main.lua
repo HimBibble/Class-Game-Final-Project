2414b79ccaa0e8c23676e1d352d481eab1b45079
HimBibble <110710030+HimBibble@users.noreply.github.com>
2026-05-03 13:03:55 -0500

    First Lines

---
+++ main.lua
@@ -0,0 +1,41 @@
+local cellSize = 12
+
+local mainGrid, gridUpdate, gridRows, gridCols
+
+math.randomseed(os.time())
+
+
+
+--Function to create the initial grid.
+local function createGrid(columns, rows)
+	local grid = {}
+	for x = 1, columns do
+		grid[x] = {}
+		for y = 1, rows do
+			grid[x][y] = 0
+		end
+	end
+	--Returns the created grid
+	return grid
+end
+
+function love.load()
+
+	--Make variables of canvas size to work with
+	local canvasWidth = love.graphics.getWidth()
+	local canvasHeight = love.graphics.getHeight()
+	
+	--Setting the grid size relative to canvas
+	gridRows = canvasWidth / cellSize
+	gridCols = canvasHeight / cellSize
+	
+	--Create starting grid
+	mainGrid = createGrid(gridCols, gridRows)
+end
+
+function love.update()
+	gridUpdate = createGrid(gridCols, gridRows)
+	
+	
+	mainGrid = gridUpdate
+end
